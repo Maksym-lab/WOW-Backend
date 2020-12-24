e8b34e3d9251c558541fd6dae203033ace71e041
Maksym-lab <maxim.sinchilo@devit-team.com>
2020-12-24 06:40:00 +0000

    refactor(routes): rename to router

--- app/app.js
+++ app/app.js
@@ -1,11 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
-const routes = require('./routes/routes');
+const router = require('./routes/router');
 const config = require('./config/config');
 const { port, limit } = config.app;
 var app = express();
 app.use(bodyParser.json({ limit }));
-routes(app);
+router(app);
 app.listen(port, function() {
   console.log('Sanity API listening on port:' + port);
 });
--- app/routes/routes.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("sequelize");
-const players = require("./players");
-var appRouter = app => {
-  app.use("/players", players);
-  app.get("*", function(req, res) {
-    res.status(404).send({ message: "404 not found" });
-  });
-};
-module.exports = appRouter;
