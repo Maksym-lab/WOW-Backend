45dfed77088b3318323f09d77d6aab3e04ae0314
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-03-06 11:36:57 +0000

    chore(CI): add shh tunnel to mysql db

--- app/models/index.js
+++ app/models/index.js
@@ -5,6 +5,14 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const config = require('../../config/config');
 const db = {};
+const mysqlPassword = process.env.MYSQL_PASSWORD;
+if (mysqlPassword.length > 0) {
+  config.sequelize.password = mysqlPassword;
+}
+const mysqlUser = process.env.MYSQL_USER;
+if (mysqlUser.length > 0) {
+  config.sequelize.username = mysqlUser;
+}
 const sequelize = new Sequelize(config.sequelize);
 const models = fs.readdirSync(__dirname).filter(file => {
   return (
