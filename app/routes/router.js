1e7477ad2507d13bc962abac09f389bf75e58833
Maksym-lab <maxim.sinchilo@devit-team.com>
2021-04-13 08:24:33 +0000

    Update router.js

--- app/routes/router.js
+++ app/routes/router.js
@@ -3,7 +3,7 @@ const players = require('./players');
 var appRouter = app => {
   app.use('/players', players);
   app.get('*', function(req, res) {
-    res.status(404).send({ message: '404 not fun' });
+    res.status(404).send({ message: '404 not found' });
   });
 };
 module.exports = appRouter;
